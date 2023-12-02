88e7af4cac7e39854e506f5dd56478ba5c1e2d05
Kaloyan Kaludov <kaloyan.kaludov@gmail.com>
2023-12-02 13:58:23 +0200

    * Add teleporters

---
+++ Assets/TeleporterEnterComponent.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterEnterComponent : MonoBehaviour
+{
+    public string TagRequired = null;
+    public GameObject TeleporterExit = null;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (TagRequired != null && TagRequired.Length > 0)
+        {
+            if (!other.CompareTag(TagRequired))
+                return;
+        }
+
+        if (!TeleporterExit)
+            return;
+
+        other.transform.SetPositionAndRotation(TeleporterExit.transform.position, other.transform.rotation);
+    }
+}
